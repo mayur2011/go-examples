ec9143f9ac32c2ff02426c4efb78e9099d2aa825
mayur2011 <er.mayur2011@gmail.com>
2021-11-07 16:59:04 +0530

    linear sorting in GO

---
+++ 3_datastructures/01_linearsort/maincopy.go
@@ -0,0 +1,20 @@
+package main
+
+// import "fmt"
+
+// func mainc() {
+// 	var x int64
+// 	var y = [5]int{}
+// 	//fmt.Printf("Enter values inputs \n")
+// 	fmt.Printf("Enter any 5 numbers :\n")
+// 	for x < 5 {
+// 		fmt.Println("value of x-", x)
+// 		fmt.Scanf("%d", &y[x])
+// 		x = x + 1
+// 	}
+// 	x = 0
+// 	for x < 5 {
+// 		fmt.Printf("Entered valaues are following- \n %d\n", y[x])
+// 		x++
+// 	}
+// }
