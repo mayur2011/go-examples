d504e689b3f65a183dc50dd9447b4db9d280b8d7
mayur2011 <er.mayur2011@gmail.com>
2021-11-07 17:32:17 +0530

    Linear Sorting

--- 3_datastructures/01_linearsort/tm01/main.go
+++ 3_datastructures/01_linearsort/tm01/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("linear sorting")
+	x := [10]int{76, 54, 60, 10, 23, 34, 21, 12, 22, 05}
+	fmt.Println("Initial List:", x)
+	size := len(x)
+	e := 0
+	for e <= (size - 2) {
+		f := e + 1
+		for f <= (size - 1) {
+			if x[f] < x[e] {
+				g := x[e]
+				x[e] = x[f]
+				x[f] = g
+			}
+			f++
+		}
+		e++
+	}
+	y := 0
+	for y <= (size - 1) {
+		fmt.Printf("%d\n", x[y])
+		y++
+	}
 }
