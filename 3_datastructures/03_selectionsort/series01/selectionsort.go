07ae93367375ae2e8d0d7195d894a1e3d8e99ad2
mayur2011 <er.mayur2011@gmail.com>
2021-11-11 01:52:23 +0530

    selection sort series

--- 3_datastructures/03_selectionsort/series01/selectionsort.go
+++ 3_datastructures/03_selectionsort/series01/selectionsort.go
@@ -5,15 +5,20 @@ import "fmt"
 func main() {
 	x := []int{75, 34, 57, 98, 23, 21, 02}
 	size := len(x)
-	//fmt.Println(size)
 	e := 0
 	for e <= (size - 2) {
-		//m := e
-		f := 1
+		m := e
+		f := e + 1
 		for f <= (size - 1) {
+			if x[f] < x[m] {
+				m = e //to capture the index of smallest element
+			}
 			fmt.Printf("%d - %d\n", e, f)
 			f++
 		}
+		g := x[e]
+		x[e] = x[m]
+		x[m] = g
 		e++
 	}
 }
