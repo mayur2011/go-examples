24518ebc05dce52ff4987461d3c1343003f7e5b0
mayur2011 <er.mayur2011@gmail.com>
2021-11-13 01:25:42 +0530

    linear sort

--- 3_datastructures/01_linearsort/practice01/linear.go
+++ 3_datastructures/01_linearsort/practice01/linear.go
@@ -4,7 +4,32 @@ import "fmt"
 
 func main() {
 	x := []int{45, 32, 76, 01, 21, 98, 01}
-
-	fmt.Println(x)
-
+	size := len(x) - 1
+	e := 0
+	for e < size {
+		f := e + 1
+		for f <= size {
+			fmt.Printf("%d %d\n", e, f)
+			f++
+		}
+		e++
+	}
 }
+
+/*
+01
+02
+03
+04
+05
+06
+12
+13
+14
+15
+16
+..
+..
+..
+56
+*/
