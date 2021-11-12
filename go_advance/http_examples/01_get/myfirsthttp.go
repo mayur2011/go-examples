9ca7d36219a01901b8bf128f20293161979d0b8b
mayur2011 <er.mayur2011@gmail.com>
2021-11-13 01:47:35 +0530

    http advance

---
+++ go_advance/http_examples/01_get/myfirsthttp.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	httpUrl := "https://api.github.com"
+
+	client := http.Client{}
+	response, err := client.Get(httpUrl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+	bytes, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(response.StatusCode)
+	fmt.Println(string(bytes))
+
+}
