cd84a207cad0c34232b4e26740ff1187bbe5ae09
mayur2011 <er.mayur2011@gmail.com>
2022-01-15 08:28:03 +0530

    Radix Sort Algo

--- 4_commonlogics/main.go
+++ 4_commonlogics/main.go
@@ -6,32 +6,31 @@ import (
 )
 
 func main() {
-	data := []int{45, 654, 769, 1200, 110, 99}
+	data := []int{450, 654, 769, 10200, 9099, 110, 99}
 	fmt.Println(data)
+	x := data
 	queue := [10][10]int{}
 	rear := [10]int{}
 	front := [10]int{}
-	x := [10]int{}
 	e := 1
 	f := 10
 
-	largest, _ := common.GetLargestNum(data)
+	largest, _ := common.GetLargestNum(x)
 	//fmt.Printf("Largest value is %d and at index of list[%d]\n", largest, index)
 	dc := common.GetDigitCount(largest)
 	//fmt.Printf("%d", dc)
 
 	y := 0
 	for y < dc {
-
 		//logic to spread the data to respective queues
 		i := 0
-		for i < len(data) {
-			num := (data[i] % f) / e
-			queue[num][rear[num]] = data[i]
+		for i < len(x) {
+			num := (x[i] % f) / e
+			queue[num][rear[num]] = x[i]
 			rear[num]++
 			i++
 		}
-		fmt.Println(queue)
+		//fmt.Println(queue)
 
 		//logic to collect back the data to an array
 		i = 0
