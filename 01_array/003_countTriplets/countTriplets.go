b1f5c732b6c00840651852f672255f16ef33aafa
mayur2011 <er.mayur2011@gmail.com>
2022-11-16 21:50:25 +0530

    count Triplets

--- 01_array/003_countTriplets/countTriplets.go
+++ 01_array/003_countTriplets/countTriplets.go
@@ -7,10 +7,17 @@ import (
 
 var p = fmt.Printf
 
+func countTriplets(arr []int, s int) int {
+	return 0
+}
+
 func main() {
 	arr := []int{7, 2, 5, 4, 1, 3}
+	size := len(arr)
 	p("size: %d\n", len(arr))
 	//p("size: %d", cap(arr))
 	sort.Ints(arr)
 	p("%d", arr)
+	c := countTriplets(arr, size)
+	p("%v", c)
 }
