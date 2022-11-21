2c1e01e54db0f355bd3f97a0dd014424e3c23182
mayur2011 <er.mayur2011@gmail.com>
2022-11-21 22:56:19 +0530

    logdir flag

--- logging/filelogrotate/main.go
+++ logging/filelogrotate/main.go
@@ -1,47 +1,42 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"time"
 
 	rotatelogs "github.com/lestrrat/go-file-rotatelogs"
 	log "github.com/sirupsen/logrus"
 )
 
-func initiLogger() {
-	//fpath := "C:\\Users\\Mayur_Upadhyay\\dev\\logs\\access_log%Y%m%d%H%M%S"
-	path := "/home/mayur/godev/logs/access.log"
-	//f, err := os.OpenFile("n", os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
-	// writer, err := rotatelogs.New(
-	// 	fmt.Sprintf("%s.%s", path, "%Y-%m-%d.%H:%M:%S"),
-	// 	rotatelogs.WithLinkName("/var/log/service/current"),
-	// 	rotatelogs.WithMaxAge(time.Second*10),
-	// 	rotatelogs.WithRotationTime(time.Second*1),
-	// )
-	//
-
+func initLogger(logDir *string) {
+	fPath := ""
+	if *logDir != "" {
+		fPath = fmt.Sprintf("%vaccess.log", *logDir)
+	} else {
+		wd, _ := os.Getwd()
+		fPath = wd + "-access.log"
+	}
 	rl, err := rotatelogs.New(
-		fmt.Sprintf("%s%s", path, "%Y%m%d%H%M%S"),
-		//rotatelogs.WithLinkName("/home/mayur/godev/logs/current"),
-		rotatelogs.WithLinkName(path),
+		fmt.Sprintf("%s.%s", fPath, "%Y%m%d%H%M%S"),
+		rotatelogs.WithLinkName(fPath),
 		rotatelogs.WithRotationTime(time.Second*5),
+		rotatelogs.WithMaxAge(7),
 	)
 
 	if err != nil {
 		fmt.Println("ERROR -\n", err)
-		//log.Fatalf("Failed to Initialize Log File %s", err)
 		panic(err)
 	}
-	fmt.Println("No Error with log writer")
-	//log.SetOutput(writer)
 	log.SetOutput(rl)
-	fmt.Println("DONE with log.SetOutput	")
-
 }
 
 func main() {
-	initiLogger()
-	for i := 0; i <= 100; i++ {
+	logDir := flag.String("logdir", "", "logging directory")
+	flag.Parse()
+	initLogger(logDir)
+	for i := 0; i <= 30; i++ {
 		fmt.Println("i =", i)
 		log.Info("PRINTING i = ", i)
 		if i%5 == 0 {
