715f704b6fcb1d7da274bc6e7cf5cdf3f8711820
mayur2011 <er.mayur2011@gmail.com>
2023-06-20 21:37:08 +0530

    controller modified

--- 11_http_examples/04_middleware/controller/usercontroller.go
+++ 11_http_examples/04_middleware/controller/usercontroller.go
@@ -1,8 +1,11 @@
 package controller
 
 import (
+	"encoding/json"
 	"go-examples/11_http_examples/04_middleware/domain"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type UserController struct {
@@ -12,17 +15,31 @@ type UserController struct {
 
 // Get user will get all users
 func (uc UserController) GetUsers(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
-
-	return domain.User{}, 0, nil
+	users, err := uc.Store.GetAll()
+	if err != nil {
+		return domain.User{}, http.StatusNotFound, err
+	}
+	return users, http.StatusOK, nil
 }
 
 // Post user will create a user
 func (uc UserController) PostUser(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
-	return domain.User{}, 0, nil
+	var user domain.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		return domain.User{}, http.StatusBadRequest, err
+	}
+	if err := uc.Store.Create(user); err != nil {
+		return domain.User{}, http.StatusNotFound, err
+	}
+	return user, 0, nil
 }
 
 // Delete user will delete given user
 func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
-
-	return domain.User{}, 0, nil
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if err := uc.Store.Delete(id); err != nil {
+		return domain.User{}, http.StatusNotFound, err
+	}
+	return nil, http.StatusAccepted, nil
 }
