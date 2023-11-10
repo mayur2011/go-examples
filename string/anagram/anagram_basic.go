c9387ed7fc6117a3e64da03d64e2c6b3f8054c7a
mayur2011 <er.mayur2011@gmail.com>
2023-11-11 00:12:46 +0530

    anagram in progress

--- string/anagram/anagram_basic.go
+++ string/anagram/anagram_basic.go
@@ -6,8 +6,23 @@ func main(){
 	input1 := "LISTEN"
 	//input2 := "SILENT"
 
+	//create a input1_map
+	var input1Map map[rune]int
+
+	//initialize the input1Map
+	input1Map = make(map[rune]int)
+	
+
 	for _, v:= range input1{
 		fmt.Printf("%c\n",v)
+		a, ok := input1Map[v]
+		if ok {
+			fmt.Println(a,ok)
+		}else {
+			fmt.Println("missing:",v)
+		}
+		//fmt.Println(input1Map)
 	}
+	fmt.Println(input1Map)
 
 }
