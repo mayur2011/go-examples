7460b1b519bea33cb418c4b58971c0fa8ed9bde9
mayur2011 <er.mayur2011@gmail.com>
2023-11-15 00:07:19 +0530

    sum of three integers

--- array/003_countTriplets/countTriplets.go
+++ array/003_countTriplets/countTriplets.go
@@ -7,7 +7,7 @@ import (
 
 var p = fmt.Printf
 
-func countTriplets(na []int, total int) bool {
+func countTriplets(na []int, total int) (bool, []int) {
 	sort.Ints(na)
 	p("%d\n",na)
 
@@ -18,7 +18,7 @@ func countTriplets(na []int, total int) bool {
 	    curSum := na[i]+na[l]+na[r]
 	    
 	    if curSum == total {
-		return true
+		return true, []int{na[i],na[l],na[r]}
 	    }else if curSum < total{
 		l++
 	    }else {
@@ -26,15 +26,15 @@ func countTriplets(na []int, total int) bool {
 	    }	    
 	  }
 	}
-	return false
+	return false, []int{}
 }
 
 func main() {
-	nums := []int{0, 1, 2, 4, 5, 7, 8}
+	nums := []int{9, 1, 2, 4, 5, 7, 8}
 	target := 9
 
 	//size := len(nums)
 	//p("size: %d\n", len(nums))
-	c := countTriplets(nums, target)
-	p("%v\n", c)
+	c, res := countTriplets(nums, target)
+	p("%v = %v\n", c, res)
 }
