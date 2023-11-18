418d9058ad7905b042ae21e92a435557f16c9689
mayur2011 <er.mayur2011@gmail.com>
2023-11-18 12:45:56 +0530

    Linked List - getLengthOfList

--- 3_datastructures/linked_list/singlylinklist.go
+++ 3_datastructures/linked_list/singlylinklist.go
@@ -1,17 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+        "os"
+)
 
 type Node struct {
-	data int
+	data string
 	next *Node
 }
 
+var l int
 var start *Node
 
-func (n *Node) add(value int) error {
+func (n *Node) add(value string) error {
 	
-	node:= &Node{data:value}
+	node := &Node{data:value}
 
 	if start == nil {
 		start = node
@@ -26,18 +30,59 @@ func (n *Node) add(value int) error {
 	return nil
 }
 
+func (n *Node) getLengthOfList() int{
+	i := 0
+	if start == nil {
+		return i
+	}
+	
+	curNode := start
+	for curNode != nil{
+		i++
+		curNode= curNode.next
+	}
+	return i
+}
+
 func (n *Node) traverse() {
 	curNode := start
 	for curNode != nil{
-		fmt.Printf("[%d, %p]\n",curNode.data,curNode.next)
+		if curNode.next !=nil {
+			fmt.Printf("[%v, %p]-->",curNode.data,curNode.next)
+		}else{
+			fmt.Printf("[%v, %p]\n", curNode.data, curNode.next)
+		}
+
 		curNode = curNode.next
 	}
 }
 
+func (n *Node) getLastNode() *Node{
+	curNode:= start
+	for curNode != nil{
+		if curNode.next == nil {
+			return curNode.next
+		}
+		curNode=curNode.next
+	}
+	
+	return curNode
+}
+
 func main(){
-		n:= &Node{}
-		n.add(1)
-		n.add(2)
-		n.add(3)
+		if len(os.Args) < 2 {
+			fmt.Println("looking for command line arguments /input like go run ... 1 2 etc !")
+			os.Exit(0)
+		}
+		
+		n := &Node{}
+
+		for i:=1; i<len(os.Args); i++ {
+			a:= os.Args[i]
+			fmt.Printf("adding data i= %v\n",a)
+			n.add(a)
+		}
 		n.traverse()
+		ln := n.getLengthOfList()
+		fmt.Println("lenght:",ln)
 }
